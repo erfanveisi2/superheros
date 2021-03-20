77c95f5853a903747eed71dc86cd9324f567b47d
erfan veisi <erfanveisi2@gmail.com>
2021-03-20 17:00:10 +0300

    render function added in App file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super()
 
-    this.state={
-      monsters:[],
-      searchField:''
+    this.state = {
+      monsters: [],
+      searchField: ''
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
-    .then(response=>response.json())
-    .then(response=>this.setState({monsters:response}))
+      .then(response => response.json())
+      .then(response => console.log(response))
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        
+      </div>
+    )
   }
 }
 
