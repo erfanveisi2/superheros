697422137d9168f082e4ce6d0093ea3e913bcafb
erfan veisi <erfanveisi2@gmail.com>
2021-06-27 19:16:15 +0300

    images are update

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css'
 
 export const Card = (props)=>(
   <div className='card-container'>
-    <img alt='superhero' src={`${props.superhero.images.md}`}/>
+    <img className='images' alt='superhero' src={`${props.superhero.images.md}`}/>
     <h2>{props.superhero.name}</h2>
     <p>power: {props.superhero.powerstats.power}</p>
     <p>speed: {props.superhero.powerstats.speed}</p>
